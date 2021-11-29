71dc79d381c3bbf38d7a8ac6f30f455948be0d29
Mohamed-Anass-Riyadi <92917452+Mohamed-Anass-Riyadi@users.noreply.github.com>
2021-11-29 22:29:05 +0100

    add

--- Réalisation/realisation.js
+++ Réalisation/realisation.js
@@ -1,28 +1,27 @@
-// declaration des variables 
 var btn;
 var output;
 var number;
 var nombreDeviner;
 var attempt;
 
- // entrer: saiser 
+
  btn = document.getElementById('btn');
  output = document.getElementById('outputtext');
  number = Math.floor(Math.random() * 100);
  console.log(number)
  attemptOutPut = document.getElementById('tentative')
  attempt =10;
- // traitement 
+
 
 btn.addEventListener('click', function(){
     attempt--;
     nombreDeviner = document.getElementById('userInput').value;
     if(attempt>0){
         attemptOutPut.innerHTML=attempt +"-tentative"
-        if ( nombreDeviner == number){
-            if (nombreDeviner == number && attempt >=8 ) { 
-                output.innerHTML = 'Bravo, vous etes un Génie !'
-                }else{
+    if ( nombreDeviner == number){
+    if (nombreDeviner == number && attempt >=8 ) { 
+                output.innerHTML = 'Bravo, vous etes un Génie !!!'
+          } else{
                 if(nombreDeviner == number && attempt >=3){
                     output.innerHTML ='Félicitations, vous avez gagné après , ' +attempt +" tentative" 
                 }
@@ -31,23 +30,15 @@ btn.addEventListener('click', function(){
           } else{
             if ( nombreDeviner < number){
                 output.innerHTML = "le nombre que vous avez choisir est plus petit !"
-            }
-            else {
+          } else {
               output.innerHTML = "le nombre que vous avez choisir est plus grand !"
             }
         
           }
-    }else {
-        output.innerHTML = "Vous avez perdu !!"
+          } else {
+        output.innerHTML = "C'est raté !!"
         attemptOutPut.innerHTML= "0-tentative"
     }
-
-
-
-
-
-  
-  
 
 });
 
